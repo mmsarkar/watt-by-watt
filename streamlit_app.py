a8c1aa7f55fd75a8b91c2075552e0279d4b55df0
mmsarkar <117538441+mmsarkar@users.noreply.github.com>
2023-02-25 14:55:16 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,17 @@
 import streamlit as st
 
-st.title("     Watt     by     Watt     ")
-st.write("This is my new app")
-button1 = st.button("click me")
+st.title("Watt by Watt")
+st.write("Select your town for more stats!")
+
+button1 = st.button("Los Angeles")
 if(button1):
-  sf.write("this is some text.")
+  sf.write("You are from LA!")
+  
+button2 = st.button("Irvine")
+if(button2):
+  sf.write("You are from Irvine!")
+  
+button3 = st.button("Riverside")
+if(button3):
+  sf.write("You are from Riverside!")
+
