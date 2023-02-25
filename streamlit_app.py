f4c0f806a47b3b36b5031d81753a6ad9a342dfea
mmsarkar <117538441+mmsarkar@users.noreply.github.com>
2023-02-25 14:52:41 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-st.title("Streamlit Tutorial App")
+
+st.title("     Watt     by     Watt     ")
 st.write("This is my new app")
 button1 = st.button("click me")
 if(button1):
