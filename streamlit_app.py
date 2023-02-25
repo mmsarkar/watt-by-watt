df6631a14482a8f98752ff0dd5d5e033f8274cb8
mmsarkar <medhamsarkar@gmail.com>
2023-02-25 15:30:37 -0800

    please I don't know what's happening

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,12 @@ st.write("Select your town for more stats!")
 
 button1 = st.button("Los Angeles")
 if(button1):
-  sf.write("You are from LA!")
+  st.write("You are from LA!")
   
 button2 = st.button("Irvine")
 if(button2):
-  sf.write("You are from Irvine!")
+  st.write("You are from Irvine!")
   
 button3 = st.button("Riverside")
 if(button3):
-  sf.write("You are from Riverside!")
+  st.write("You are from Riverside!")
