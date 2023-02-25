ba17acbd4f734e8a87d90c9dc1f9fadb904edb15
mmsarkar <117538441+mmsarkar@users.noreply.github.com>
2023-02-25 14:47:41 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
-
-print("hello world")
+import streamlit as st
+st.title("Streamlit Tutorial App")
+st.write("This is my new app")
+button1 = st.button("click me")
+if(button1):
+  sf.write("this is some text.")
