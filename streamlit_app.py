1fef955acc0ae45a2f215d5a67ea21d21703704f
mmsarkar <medhamsarkar@gmail.com>
2023-02-25 15:24:01 -0800

    please I don't know what's happening

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,16 @@
+import streamlit as st
 
-print("hello world")
+st.title("Watt by Watt")
+st.write("Select your town for more stats!")
+
+button1 = st.button("Los Angeles")
+if(button1):
+  sf.write("You are from LA!")
+  
+button2 = st.button("Irvine")
+if(button2):
+  sf.write("You are from Irvine!")
+  
+button3 = st.button("Riverside")
+if(button3):
+  sf.write("You are from Riverside!")
